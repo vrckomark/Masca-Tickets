f9ff40f1ec54e4b5eb2b3e665daa1400efcb81e7
Vrechko <vrckomark@gmail.com>
2024-08-24 22:07:50 +0200

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,33 +10,16 @@ function App() {
     const fetchData = async () => {
       const response = await fetch("http://localhost:3000/health-check");
       const data = await response.json();
-      console.log(data, response);
+      console.log(data);
     };
     fetchData();
   }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold border-2 rounded-xl p-4">
+        Boilerplate
+      </h1>
     </>
   );
 }
