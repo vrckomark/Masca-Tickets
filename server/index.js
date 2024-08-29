cfe625753b6614e4788bf33b5853aea185170f64
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-08-29 13:14:29 +0200

    setup mongodb Atlas on backend & npm to pnpm

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-
-require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
@@ -16,3 +16,9 @@ app.get("/health-check", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+/* TEST MONGODB connect */
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
