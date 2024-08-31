f4850ac857be9f8fd3d69fa43f693588b574003f
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-08-31 15:00:44 +0200

    Masca connect

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,29 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import "@rainbow-me/rainbowkit/styles.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
-import App from './App';
-import { config } from './wagmi';
+import App from "./App";
+import { config } from "./wagmi";
+import { MascaProvider } from "./MascaProvider";
 
-import './index.css'
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
+      <MascaProvider>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <App />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </MascaProvider>
     </WagmiProvider>
-  </StrictMode>,
+  </StrictMode>
 );
--- client/src/pages/Vendor.tsx
+++ client/src/pages/Vendor.tsx
@@ -1,19 +1,18 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useMasca } from '../MascaProvider';
 
 function Vendor() {
+  const { currentDID, currentDIDMethod } = useMasca();
+
   return (
     <>
-      <h1 className="text-3xl font-bold border-2 rounded-xl p-4">
-        <a href="/user">Vendor page</a>
-      </h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          padding: 12,
-        }}
-      >
+      <h1 className="text-3xl font-bold border-2 rounded-xl p-4">Vendor Page</h1>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: 12 }}>
         <ConnectButton />
+      </div>
+      <div>
+        <p><strong>Current DID:</strong> {currentDID || 'Not connected'}</p>
+        <p><strong>Current DID Method:</strong> {currentDIDMethod || 'Not connected'}</p>
       </div>
     </>
   );
